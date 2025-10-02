dc9f7f8bc6832e387edee9aecdb383e12ff60dd6
Tushar-1013 <146510989+Tushar-1013@users.noreply.github.com>
2025-10-02 23:35:21 +0530

    Delete WTP directory

--- WTP/js/script.js
+++
@@ -1,8 +0,0 @@
-let workexp = document.getElementById("workexp");
-let optional = document.querySelectorAll(".optional")
-
-function show() {
-
-    (workexp.value == "yes") ? optional.forEach((e) => e.style.display = "") :
-        optional.forEach((e) => e.style.display = "none");
-}
